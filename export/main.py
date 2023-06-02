b0e15d9f81a8fa5b4f5b62f9596a379d7f1ae2c5
majent4 <99695392+majent4@users.noreply.github.com>
2023-06-02 18:42:35 +0200

    export: main conditions arch

--- export/main.py
+++ export/main.py
@@ -1,6 +1,19 @@
-from sys import exit as sys_exit
+from sys import argv as sys_argv, exit as sys_exit
+from os.path import isfile
+from utils import print_exit
+from sqlite_database import SQLiteDatabase
 
 def main():
+    if len(sys_argv) == 3 and isfile(sys_argv[1]):
+        xml_file = sys_argv[1]
+        db_file = sys_argv[2]
+    else:
+        print_exit('Invalid or missing argument(s)')
+    db_exists = isfile(db_file)
+    db = SQLiteDatabase(db_file)
+    if not db_exists:
+        db.commit()
+    db.close()
     return 0
 
 if __name__ == '__main__':
